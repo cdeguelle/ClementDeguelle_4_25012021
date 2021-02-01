8a14f7ab30e901fa3ccaedb18693aadaf7ad5e25
Clément Deguelle <clement.deguelle@hotmail.com>
2021-02-01 16:57:11 +0100

    Issue 1

---
+++ starterOnly/modal.js
@@ -0,0 +1,32 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll("#close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
